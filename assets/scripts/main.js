104151cdcc1eca91091d2ec11c51e34c5beae321
Ren Tamdang <tamdangren@gmail.com>
2025-05-16 15:39:08 +0800

    Separating into scenes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-var config = {
+const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
@@ -9,7 +9,7 @@ var config = {
             debug: false
         }
     },
-    scene: []
+    scene: Level1
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
